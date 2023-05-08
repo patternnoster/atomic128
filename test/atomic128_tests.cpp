9b677f42f0b7752af3b9a90f5fdd7dbffebcd144
patternnoster <retsonnrettap@gmail.com>
2023-05-08 21:28:24 +0300

    Basic test on CAS

--- test/atomic128_tests.cpp
+++ test/atomic128_tests.cpp
@@ -1,1 +1,36 @@
+#include <gtest/gtest.h>
+
 #include "atomic128/atomic128_ref.hpp"
+
+using namespace atomic128;
+using std::size_t;
+
+struct alignas(16) vec {
+  vec *ptr;
+  uint64_t ctr;
+  bool operator==(const vec&) const = default;
+};
+
+TEST(Atomic128RefTests, cas) {
+  constexpr size_t Cycles = 100;
+
+  vec mem = { nullptr, 0 };
+  vec prev = { &mem, 42 };
+  for (size_t i = 0; i < Cycles; ++i) {
+    vec orig_good = mem;  // Non-atomic loads
+    vec orig_bad = prev;
+
+    prev = mem;
+
+    vec next = { mem.ptr + 1, mem.ctr + 1 };
+    atomic128_ref ref(mem);
+
+    ASSERT_FALSE(ref.compare_exchange_weak(orig_bad, next));
+    ASSERT_EQ(mem, orig_good);  // No change
+    ASSERT_EQ(orig_bad, prev);  // Load old value
+
+    ASSERT_TRUE(ref.compare_exchange_weak(orig_good, next));
+    ASSERT_EQ(mem, next);
+    ASSERT_EQ(orig_good, prev);
+  }
+}
