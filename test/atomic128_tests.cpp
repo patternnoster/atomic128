c6573f0045d130384c2aa3e94e1184a8dc2db4fd
patternnoster <retsonnrettap@gmail.com>
2023-05-15 21:02:45 +0300

    Additional tests, including load/store/exchange

--- test/atomic128_tests.cpp
+++ test/atomic128_tests.cpp
@@ -36,3 +36,50 @@ TEST(Atomic128RefTests, cas) {
     ASSERT_EQ(orig_good, prev);
   }
 }
+
+TEST(Atomic128RefTests, cas_alternative) {
+  vec m1{ nullptr, 0 }, m2{ &m1, 1 };
+
+  // Common case: old_value == ref
+  ASSERT_TRUE(atomic128_ref(m1).compare_exchange_strong(m1, m2));
+  ASSERT_EQ(m1, m2);
+
+  // Weak interfaces
+  const vec new_val{ &m2, 2 };
+  while (!atomic128_ref(m1).compare_exchange_weak(m2, { &m2, 2 },
+                                                  std::memory_order_relaxed));
+  ASSERT_EQ(m1, new_val);
+
+  while (!atomic128_ref(m1).compare_exchange_weak(m1, m2,
+                                                  std::memory_order_acq_rel,
+                                                  std::memory_order_relaxed));
+  ASSERT_EQ(m1, m2);
+}
+
+TEST(Atomic128RefTests, lse) {
+  constexpr size_t Cycles = 100;
+
+  vec mem = { &mem, 0 };
+  for (size_t i = 0; i < Cycles; ++i) {
+    atomic128_ref ref(mem);
+
+    // I'm just having fun with the memory order args, don't look too
+    // much into it...
+
+    vec obj = i % 2 ? ref.load(std::memory_order_relaxed) : ref;
+    ASSERT_EQ(obj.ptr, &mem + 2*i);
+    ASSERT_EQ(obj.ctr, 2*i);
+
+    const vec next = { obj.ptr + 1, obj.ctr + 1 };
+    const auto prev = ref.exchange(next, std::memory_order_acq_rel);
+    ASSERT_EQ(prev, obj);
+
+    obj = ref.load(std::memory_order_seq_cst);
+    ASSERT_EQ(obj.ptr, next.ptr);
+    ASSERT_EQ(obj.ctr, next.ctr);
+
+    const vec next_next{next.ptr + 1, next.ctr + 1};
+    if (i % 2) ref.store(next_next, std::memory_order_release);
+    else ref = next_next;
+  }
+}
